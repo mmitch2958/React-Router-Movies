28197ce80006176d5e4c06ab70f048053d6ea688
mmitch2958 <mikescottmitchell@outlook.com>
2020-08-18 15:01:23 -0400

    finished

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,33 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import MovieCard from './MovieCard'
 
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
  
   useEffect(() => {
-    const id = getMovie;
-    // change ^^^ that line and grab the id from the URL
-    // You will NEED to add a dependency array to this effect hook
+    const id = this.props.match.params.id;
+    this.fetchMovie(id);
+  })
 
-      if (this.props.match.params.id !== newProps.match.params.id) {
-        this.getMovie(newProps.match.params.id);
-    }
-    
-  
-    getMovie = (id) => {
-      axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        })
-    };
-
-  },[this.getMovie(this.props.match.params.id)]);
+  fetchMovie = id => {
+    axios
+      .get(`http://localhost:5000/api/movies/${id}`)
+      .then(response => {
+        this.setState(() => ({ movie: response.data }));
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = evt => {
